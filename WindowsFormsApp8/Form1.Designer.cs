78d6645f642181f2586aecb05cc88f54d4d9b0b3
Patryk Krawczak <patryk.krawczak@lingarogroup.com>
2020-03-30 20:43:43 +0200

    Add project files.

---
+++ WindowsFormsApp8/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace WindowsFormsApp8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.z_box = new System.Windows.Forms.TextBox();
+            this.wynik_box = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(506, 344);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(189, 82);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "OBLICZ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(222, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Patryk Krawczak C# zadanie 8";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 145);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "z";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // z_box
+            // 
+            this.z_box.Location = new System.Drawing.Point(16, 168);
+            this.z_box.Name = "z_box";
+            this.z_box.Size = new System.Drawing.Size(91, 26);
+            this.z_box.TabIndex = 3;
+            // 
+            // wynik_box
+            // 
+            this.wynik_box.Location = new System.Drawing.Point(328, 95);
+            this.wynik_box.Multiline = true;
+            this.wynik_box.Name = "wynik_box";
+            this.wynik_box.Size = new System.Drawing.Size(421, 222);
+            this.wynik_box.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.wynik_box);
+            this.Controls.Add(this.z_box);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox z_box;
+        private System.Windows.Forms.TextBox wynik_box;
+    }
+}
+
---
+++ WindowsFormsApp8/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsApp;
+
+namespace WindowsFormsApp8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            
+
+
+            int z = int.Parse(z_box.Text);
+            Random rnd = new Random();
+            int d = 10;
+            int l = 100000;
+            double f = 0.0F;
+            int o = 0;
+            WindowsFormsApp.SingleCount single = new SingleCount();
+            single.X1 = 0;
+            single.X2 = Math.PI/2;
+            single.Area = Math.Sin(single.X2) - Math.Sin(single.X1);
+            bool czy1 = false;
+            bool czy2 = false;
+            for (double n = 100000.0F; n > 0  ; n -= Math.Max(n*0.1,10 ))
+            {
+
+
+                f = 0.0F;
+                int licz = 0;
+                double old_i = 0.0F;
+                for (double i = single.X1; i <= single.X2; i = i + 100F / n)
+                {
+
+                    licz += 1;
+                    f += Math.Cos(i) * (i - old_i);
+
+                    old_i = i;
+                };
+                if ((Math.Abs(single.Area - f)/ single.Area  >= z / 100.0 ) && !czy1)
+                {
+
+                    string wynik = "metoda prostokatów parametry to n=";
+                    wynik += n.ToString();
+                    wynik += "  wynik to =";
+                    wynik += f.ToString();
+                    wynik += "\t\t\t";
+                    wynik_box.Text += wynik;
+                    czy1 = true;
+                }
+                f = 0.0F;
+
+                old_i = 0.0F;
+                for (double i = single.X1; i <= single.X2; i = i + 100F / n)
+                {
+
+                    licz += 1;
+                    f += ((Math.Cos(i) + Math.Cos(old_i)) / 2) * (i - old_i);
+
+                    old_i = i;
+                };
+                if ((Math.Abs(single.Area - f)/ single.Area >= z / 100.0) && !czy2)
+                {
+                    double xxx = f;
+
+                    string wynik =  "metoda trapezów parametry to n=";
+                    wynik += n.ToString();
+                    wynik += "  wynik to =";
+                    wynik += f.ToString();
+                    wynik += "\t\t";
+                    wynik_box.Text += wynik;
+                    czy2 = true;
+                }
+                if (czy1 && czy2) 
+                {
+                    break;
+                }
+            };
+        }
+    }
+}
